ea33ed4a78bfe4add4eafea081280dfa717a1009
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-08-30 23:38:50 +0900

    finish chapter10

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'bcrypt',         '3.1.11'
 gem 'bootstrap-sass', '3.3.7'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
+gem 'faker'
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_030717) do
+ActiveRecord::Schema.define(version: 2018_08_30_142444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_08_27_030717) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
